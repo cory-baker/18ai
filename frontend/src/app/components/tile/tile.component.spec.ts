56517e8c59add7b76d934708d29f0398926b0dd6
Cory Baker <coryb08@gmail.com>
2026-06-09 19:35:13 -0500

    wip

--- frontend/src/app/components/tile/tile.component.spec.ts
+++ frontend/src/app/components/tile/tile.component.spec.ts
@@ -5,6 +5,7 @@ import { Tiles } from '@18ai/engine';
 
 describe('TileComponent', () => {
   let fixture: ComponentFixture<TileComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,15 +14,33 @@ describe('TileComponent', () => {
 
     fixture = TestBed.createComponent(TileComponent);
     fixture.componentInstance.tile = Tiles.byId('9');
+    fixture.componentInstance.rotation = 0;
     fixture.detectChanges();
+    nativeElement = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('render two <line> elements for a single-path tile (black line/white outline)', () => {
-    const lines = fixture.nativeElement.querySelectorAll('line');
-    expect(lines.length).toBe(2);
+  describe('for tile #9', () => {
+    it('renders two <line> elements for a single-path tile (black line/white outline)', () => {
+      const lines = nativeElement.querySelectorAll('line');
+      expect(lines.length).toBe(2);
+    });
+
+    it('positions lines from edge 0 (N) to edge 3 (s)', () => {
+      const lines = nativeElement.querySelectorAll('line');
+      const black = lines[1];
+      expect(black.getAttribute('y1')).toBe('-87');
+      expect(black.getAttribute('y2')).toBe('87');
+      expect(black.getAttribute('x1')).toBe('0');
+      expect(black.getAttribute('x2')).toBe('0');
+    });
+
+    it('applies rotation transform on the outer <g>', () => {
+      const g = nativeElement.querySelector('svg:g');
+      expect(g.getAttribute('transform')).toBe('rotate(0)');
+    });
   });
 });
